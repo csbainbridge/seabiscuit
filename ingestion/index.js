0083086ac1b6b27dc9d7897d5893d8e6002aaf27
Curtis Bainbridge <CurtisB@pressassociation.com>
2017-04-02 21:38:57 +0100

    Created standardizeJson module

--- ingestion/index.js
+++ ingestion/index.js
@@ -3,8 +3,10 @@
 	Module is dependant on @processXML.
 */
 var processXml = require('./ProcessXml.js');
+var standardizeJson = require('./StandardizeJson');
 
 // Export processXml as ProcessXml.
 module.exports = {
 	processXml : processXml,
+	standardizeJson : standardizeJson,
 }
