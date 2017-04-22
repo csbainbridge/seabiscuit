8366110356f58bca8bb96f7d506c52558e90d79d
Curtis Bainbridge <Curtis Bainbridge>
2017-04-22 04:31:56 +0100

    Refactored nightsWatch module (Now using hash map)

--- ingestion/nightsWatch.js
+++ ingestion/nightsWatch.js
@@ -13,8 +13,8 @@ var watchDirectories = [
 	@Dependencies
 	@fs, @underscore and @bluebird.
 */
-var fs = require('fs');
 var Promise = require('bluebird');
+var fs = Promise.promisifyAll(require('fs'));
 var _ = Promise.promisifyAll(require('underscore'));
 
 /*
@@ -22,16 +22,18 @@ var _ = Promise.promisifyAll(require('underscore'));
 	@Array statArray - Stores file stat objects {Keys: FileName, AddedTime} {Values: name of current file being processed, file stat change time}.
 	@Array liveDirectories - Stores directory objects {Keys: DirectoryName, NumOfFiles} {Values: name of directory, number of files in the given directory}.
 */
+var filesAdded = []
 var statArray = [];
 var liveDirectories = [];
+var count = 0;
 
 /*
-	@orderByTimeAdded function
 	@param files - Array of files
 	@param directory - Current directory being processed.
 	@desc - Returns an array of file stat objects sorted by the time added to the given @directory.
 */
 function orderByTimeAdded( files, directory ) {
+	statArray = [];
 	_.each(files, function getTimeAdded( file ) {
 		var fileStats = fs.statSync(directory+"/"+file)
 			statArray.push({
@@ -48,30 +50,42 @@ function orderByTimeAdded( files, directory ) {
 	@desc - Creates a new live directory object and stores it in the @liveDirectories global array.
 */
 function createLiveDirectory( directory ) {
-	return new Promise(function( resolve, reject ){
-		var newLiveDirectory = {
-			"DirectoryName" : directory.DirectoryName,
-			"NumOfFiles" : 0
-		}
-		resolve(liveDirectories.push(newLiveDirectory));
-	})
+	return liveDirectories.push({
+		"DirectoryName" : directory.DirectoryName,
+		"NumOfFiles" : 0
+	});
+}
+
+function pushToArray( array ) {
+	return function( item ) {
+		array.push(item);
+	}
+}
+
+function iterateFiles( numOfFilesAdded, fileArray, filesAdded ) {
+	fileArray.slice(0, numOfFilesAdded).forEach(pushToArray(filesAdded))
 }
 
 function getFilesAdded( directoryData ) {
-	return new Promise(function( resolve, result ) {
-		_.each(liveDirectories, function( liveDirectory ) {
-			_.each(directoryData, function( directory ) {
-				if ( directory.DirectoryName === liveDirectory.DirectoryName && directory.Files.length > liveDirectory.NumOfFiles ) {
-					var filesAdded = [];
-					numOfFilesAdded = directory.Files.length - liveDirectory.NumOfFiles;
-					fileArray = orderByTimeAdded(directory.Files, directory.DirectoryName);
-					for (var i = numOfFilesAdded; i > 0; i--) {
-						filesAdded.push((fileArray[i - 1]))
-					}
-					liveDirectory.NumOfFiles = directory.Files.length;
-					resolve(_.sortBy(filesAdded, 'FileName'))
-				}
-			})
+	return new Promise(function( resolve, reject ) {
+		var dirMap = new Map()
+		_.each(liveDirectories, function( liveDirectory) {
+			dirMap.set(liveDirectory.DirectoryName, liveDirectory)
+		})
+		_.each(directoryData, function( directory ) {
+			var liveDir = dirMap.get(directory.DirectoryName)
+			if ( directory.Files.length > liveDir.NumOfFiles && count > 0) {
+				var fileArray = [];
+				filesAdded = [];
+				numOfFilesAdded = directory.Files.length - liveDir.NumOfFiles
+				fileArray = orderByTimeAdded(directory.Files, directory.DirectoryName);
+				iterateFiles(numOfFilesAdded, fileArray, filesAdded)
+				liveDir.NumOfFiles = directory.Files.length;
+				count++
+				resolve(_.sortBy(filesAdded, 'FileName'))
+			} else {
+				count++
+			}
 		})
 	})
 }
@@ -80,17 +94,8 @@ function checkDirectoryLength( directoryData ) {
 	return new Promise(function( resolve, reject ) {
 		if ( liveDirectories.length === 0 ) {
 			directoryData.forEach(createLiveDirectory)
-			.catch(function( error ) {
-				return
-			})
 		} else if ( liveDirectories.length > 0 ) {
-			getFilesAdded(directoryData)
-			.then(function(files){
-				resolve(files)
-			})
-			.catch(function(error){
-				return
-			})
+			resolve(getFilesAdded(directoryData))
 		}
 	})
 }
@@ -101,17 +106,11 @@ function checkDirectoryLength( directoryData ) {
 	@desc - Returns an object containing the @directory and an array of files it contains.
 	*/
 function readDirectory( directory ) {
-	return new Promise(function( resolve, reject ) {
-		fs.readdir(directory, function( error, files ) {
-			if ( error ) {
-				reject(error)
-				return
-			}
-			resolve({
-				"DirectoryName" : directory,
-				"Files" : files,
-			})
-		})
+	return fs.readdirAsync(directory)
+	.then(function( files ) {
+		return { "DirectoryName" : directory, "Files" : files, }	
+	}).catch(function( error ){
+		return error
 	})
 }
 /*
@@ -144,4 +143,4 @@ function watcherOnTheWall(watchDirectories) {
 	}
 }
 
-setInterval(watcherOnTheWall(watchDirectories), 100);
+setInterval(watcherOnTheWall(watchDirectories), 1000);
