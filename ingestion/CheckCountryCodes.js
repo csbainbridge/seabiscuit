18fa71b3428d178e7e9b732d0995e6410cbf9b1a
Curtis Bainbridge <CurtisB@pressassociation.com>
2017-04-23 13:52:31 +0100

    Created CheckCountryCodes module

---
+++ ingestion/CheckCountryCodes.js
@@ -0,0 +1,28 @@
+var Promise = require('bluebird'),
+    setSABettingValues = require('./SetSABettingValues.js')
+
+module.exports = {
+    /**
+	 * Calls setter function based on the supplied datas country code.
+	 * 
+	 * @param {Object} bettingDataObjects The object holds the betting data supplied, country code and pa betting object.
+	 * @returns {Object} Returns pa betting object.
+	 */
+    checkCountryCode : function( bettingDataObjects ) {
+        return new Promise(function( resolve, reject ) {
+            var paObject = bettingDataObjects.PAObject,
+                bettingObject = bettingDataObjects.BettingDataObject,
+                messageType = bettingObject.HorseRacingX.Message["0"].$.type,
+                countryCode = bettingDataObjects.CountryCode
+            if (typeof countryCode===undefined){
+                reject({
+                    "Error" : "Country Code is undefined",
+                    "Action" : "N/A"
+                })
+            }
+            if ( countryCode === "SA" ) {
+                resolve(setSABettingValues.checkMessageValues(messageType, paObject, bettingObject))
+            }
+        })
+    }
+}
